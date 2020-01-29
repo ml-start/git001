b818f12b2456a6ceb40059a959651bfa0380f23d
ml-start <3386441921@qq.com>
2020-01-29 20:28:52 +0800

    分支

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class Test{
+
+  public static void main(String[] args) {
+	  int a=130;
+	  byte b=(byte)a;
+	  System.out.println("b="+b);
+	int in=1;
+   System.out.println("仙履奇缘\t月光宝盒\t大圣娶亲\n\t喜剧之王\t功夫");
+  }
+}
